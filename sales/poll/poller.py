1e10aabe52a3e935d9713363ed5166e0df2e9b32
Alexander Rubin <your@email.here>
2024-02-06 17:46:21 -0500

    backend done, code clean

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -16,15 +16,10 @@ def get_automobile():
     url = 'http://project-beta-inventory-api-1:8000/api/automobiles/'
     response = requests.get(url)
     content = json.loads(response.content)
-    print("just polled")
-    print("AutomobileVO", AutomobileVO.objects.all())
-    print("content", content)
     for automobile in content['autos']:
-        print("automobile", automobile)
         AutomobileVO.objects.update_or_create(
             vin=automobile['vin']
         )
-    print(len(AutomobileVO.objects.all()), "flibbyflabby")
 def poll():
     while True:
         print('Sales poll poll poll poll polling')
