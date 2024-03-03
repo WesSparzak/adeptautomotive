b7979149eca50e51ad182689eadd027f2a545ab3
Wesley Sparzak <wessparzak@gmail.com>
2024-03-03 18:02:16 -0500

    Attempted to install and import GSAP, WIP

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,11 +1,24 @@
 import adept_automotive_logo from "./images/adept_automotive_logo.jpeg";
+// import { useRef } from "react";
+// import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+
+
+// #### all commented out area is attempt to use GSAP, work in progress ####
 
 function MainPage() {
+
+  // const box = useRef();
+
+  // useGSAP(() => {
+  //   gsap.to(box.curent, {x:360});
+  // });
+
   return (
     <div className="mainPageBody">
       <h1 className="display-5 fw-bold"><img src={adept_automotive_logo} alt="Adept Automotive Logo" className="mainLogo" /></h1>
       <div className="col-lg-6 mx-auto">
-        <p className="mainpageParagraph">
+        <p  className="mainpageParagraph">
           If you're buying a car, you want someone with talent. <br />
           <br />
           Someone, adept. <br />
@@ -17,6 +30,7 @@ function MainPage() {
           in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
           sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
           mollit anim id est laborum. <br />
+
         </p>
       </div>
     </div>
